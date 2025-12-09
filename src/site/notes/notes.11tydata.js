aa2a5aebad813ef17e1516608099b4cfe7262759
xiaoqf <619712271@qq.com>
2025-12-09 23:52:11 +0800

    Update notes.11tydata.js
    确保只有文件名是 home.md 的文件才能使用根路径永久链接

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -6,28 +6,18 @@ const allSettings = settings.ALL_NOTE_SETTINGS;
 module.exports = {
   eleventyComputed: {
     layout: (data) => {
-      if (data.tags.indexOf("gardenEntry") != -1) {
+      if (data.tags && data.tags.indexOf("gardenEntry") != -1) {
         return "layouts/index.njk";
       }
       return "layouts/note.njk";
     },
     permalink: (data) => {
-      if (data.tags.indexOf("gardenEntry") != -1) {
+      // 确保只有文件名是 home.md 的文件才能使用根路径永久链接
+      if (data.tags && data.tags.indexOf("gardenEntry") != -1 && data.page.fileSlug === "home") {
         return "/";
       }
       return data.permalink || undefined;
     },
-    settings: (data) => {
-      const noteSettings = {};
-      allSettings.forEach((setting) => {
-        let noteSetting = data[setting];
-        let globalSetting = process.env[setting];
-
-        let settingValue =
-          noteSetting || (globalSetting === "true" && noteSetting !== false);
-        noteSettings[setting] = settingValue;
-      });
-      return noteSettings;
-    },
+    // ...
   },
 };
