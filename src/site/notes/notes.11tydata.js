648d2c64b78c5981223395dafe6838ab2b6270d8
xiaoqf <619712271@qq.com>
2025-12-10 01:13:13 +0800

    Update notes.11tydata.js
    恢复

--- src/site/notes/notes.11tydata.js
+++ src/site/notes/notes.11tydata.js
@@ -6,22 +6,16 @@ const allSettings = settings.ALL_NOTE_SETTINGS;
 module.exports = {
   eleventyComputed: {
     layout: (data) => {
-      if (data.tags && data.tags.indexOf("gardenEntry") != -1) {
+      if (data.tags.indexOf("gardenEntry") != -1) {
         return "layouts/index.njk";
       }
       return "layouts/note.njk";
     },
     permalink: (data) => {
-      // 确保只有文件名是 home.md 的文件才能使用根路径永久链接
-      if (data.tags && data.tags.indexOf("gardenEntry") != -1 && data.page.fileSlug === "home") {
+      if (data.tags.indexOf("gardenEntry") != -1) {
         return "/";
       }
-      // 如果设置了 dg-path，使用它作为基础路径
-      if (data.dgPath) {
-        return `${data.dgPath}/${data.page.fileSlug}/`;
-      }
-      // 默认使用 notes 目录
-      return `notes/${data.page.fileSlug}/`;
+      return data.permalink || undefined;
     },
     settings: (data) => {
       const noteSettings = {};
